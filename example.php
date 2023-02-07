91d97a700b19676864c8c21559addc1dffe9ed4d
Brandon kwamou <brandonkwamou@gmail.com>
2023-02-07 20:25:32 +0100

    Strings methods

---
+++ example.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Name</title>
+</head>
+
+<body>
+    <?php
+    define("valueOfPi", 3.14);
+    define("valueOfGravity", 9.8);
+
+    echo "PI Value: " . valueOfPi;
+    echo "Gravity Value: " . valueOfGravity;
+
+    ?>
+</body>
+
+</html>
